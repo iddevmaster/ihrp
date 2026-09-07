099a7c1d12d057b8803ec43e52c638d237234848
iddevmaster <itservices@iddrives.co.th>
2026-09-07 09:57:20 +0700

    x
    x

--- views/result-document/_wrap.php
+++ views/result-document/_wrap.php
@@ -3,7 +3,7 @@ $this->registerCssFile("@web/css/main.css");
 ?>
 <html>
 
-<body style="font-family: 'TH SarabunPSK' !important; font-size: 20px !important;">
+<body style="font-family: Tahoma, sans-serif !important; font-size: 20px !important;">
   <?= $content ?>
 </body>
 
